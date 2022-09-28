328716673d4b137937247408d9e5cc0a496a3740
Optischa <fabian.evers2602@gmail.com>
2022-09-28 16:29:08 +0200

    Update sidebar

--- frontend/components/sidebar.tsx
+++ frontend/components/sidebar.tsx
@@ -4,81 +4,71 @@ import Link from "next/link";
 export default function Sidebar() {
     return (
         <>
-                <span
-                    className="absolute text-white text-4xl top-5 left-4 cursor-pointer"
+                <input type="checkbox" id="menu-open" className="hidden"/>
 
-                >
-      <i className="bi bi-filter-left px-2 bg-gray-900 rounded-md"></i>
-    </span>
-            <div
-                className="sidebar fixed top-0 bottom-0 lg:left-0 left-[-300px] p-2 w-[300px] overflow-y-auto text-center bg-gray-900"
-            >
-                <div className="text-gray-100 text-xl">
-                    <div className="p-2.5 mt-1 flex items-center">
-                        <i className="bi bi-app-indicator px-2 py-1 rounded-md bg-blue-600"></i>
-                        <h1 className="font-bold text-gray-200 text-[15px] ml-3">TailwindCSS</h1>
-                        <i
-                            className="bi bi-x cursor-pointer ml-28 lg:hidden"
+                <header className="bg-gray-600 dark:bg-[#1a1c23] text-gray-100 flex justify-between md:hidden"
+                        data-dev-hint="mobile menu bar">
+                    <a href="#" className="block p-4 text-white font-bold whitespace-nowrap truncate">
+                        URL-Shorter
+                    </a>
 
-                        ></i>
+                    <label htmlFor="menu-open" id="mobile-menu-button"
+                           className="m-2 p-2 focus:outline-none hover:text-white hover:bg-gray-700 rounded-md">
+                        <svg id="menu-open-icon" className="h-6 w-6 transition duration-200 ease-in-out"
+                             xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                  d="M4 6h16M4 12h16M4 18h16"/>
+                        </svg>
+                        <svg id="menu-close-icon" className="h-6 w-6 transition duration-200 ease-in-out"
+                             xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                  d="M6 18L18 6M6 6l12 12"/>
+                        </svg>
+                    </label>
+                </header>
+
+                <aside id="sidebar"
+                       className="bg-gray-800 dark:bg-[#1a1c23] text-gray-100 md:w-64 w-3/4 space-y-6 pt-6 px-0 absolute inset-y-0 left-0 transform md:relative md:translate-x-0 transition duration-200 ease-in-out  md:flex md:flex-col md:justify-between overflow-y-auto"
+                       data-dev-hint="sidebar; px-0 for frameless; px-2 for visually inset the navigation">
+                    <div className="flex flex-col space-y-6"
+                         data-dev-hint="optional div for having an extra footer navigation">
+                        <a href="#" className="text-white flex items-center space-x-2 px-4" title="URL-Shorter">
+                            <span className="text-2xl font-extrabold whitespace-nowrap truncate">URL-Shorter</span>
+                        </a>
+
+                        <nav data-dev-hint="main navigation">
+                            <a href="#"
+                               className="flex items-center space-x-2 py-2 px-4 transition duration-200 hover:bg-gray-700 hover:text-white">
+                                <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" fill="none"
+                                     viewBox="0 0 24 24" stroke="currentColor">
+                                    <path d="M12 14l9-5-9-5-9 5 9 5z"/>
+                                    <path
+                                        d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"/>
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                          d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"/>
+                                </svg>
+                                <span>About</span>
+                            </a>
+                            <a href="#"
+                               className="flex items-center space-x-2 py-2 px-4 transition duration-200 hover:bg-gray-700 hover:text-white">
+                                <span className="ml-6">Without Icon</span>
+                            </a>
+                            <a href="#"
+                               className="flex items-center space-x-2 py-2 px-4 transition duration-200 hover:bg-gray-700 hover:text-white group">
+                                <span
+                                    className="w-4 h-4 flex-shrink-0 border border-gray-600 rounded group-hover:border-gray-400 transition duration-200"></span>
+                                <span>Without Icon And a bit longer than usual</span>
+                            </a>
+                        </nav>
                     </div>
-                    <div className="my-2 bg-gray-600 h-[1px]"></div>
-                </div>
-                <div
-                    className="p-2.5 flex items-center rounded-md px-4 duration-300 cursor-pointer bg-gray-700 text-white"
-                >
-                    <i className="bi bi-search text-sm"></i>
-                    <input
-                        type="text"
-                        placeholder="Search"
-                        className="text-[15px] ml-4 w-full bg-transparent focus:outline-none"
-                    />
-                </div>
-                <div
-                    className="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600 text-white"
-                >
-                    <i className="bi bi-house-door-fill"></i>
-                    <span className="text-[15px] ml-4 text-gray-200 font-bold">Home</span>
-                </div>
-                <div
-                    className="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600 text-white"
-                >
-                    <i className="bi bi-bookmark-fill"></i>
-                    <span className="text-[15px] ml-4 text-gray-200 font-bold">Bookmark</span>
-                </div>
-                <div className="my-4 bg-gray-600 h-[1px]"></div>
-                <div
-                    className="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600 text-white"
-                >
-                    <i className="bi bi-chat-left-text-fill"></i>
-                    <div className="flex justify-between w-full items-center">
-                        <span className="text-[15px] ml-4 text-gray-200 font-bold">Chatbox</span>
-                        <span className="text-sm rotate-180" id="arrow">
-            <i className="bi bi-chevron-down"></i>
-          </span>
-                    </div>
-                </div>
-                <div
-                    className="text-left text-sm mt-2 w-4/5 mx-auto text-gray-200 font-bold"
-                    id="submenu"
-                >
-                    <h1 className="cursor-pointer p-2 hover:bg-blue-600 rounded-md mt-1">
-                        Social
-                    </h1>
-                    <h1 className="cursor-pointer p-2 hover:bg-blue-600 rounded-md mt-1">
-                        Personal
-                    </h1>
-                    <h1 className="cursor-pointer p-2 hover:bg-blue-600 rounded-md mt-1">
-                        Friends
-                    </h1>
-                </div>
-                <div
-                    className="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600 text-white"
-                >
-                    <i className="bi bi-box-arrow-in-right"></i>
-                    <span className="text-[15px] ml-4 text-gray-200 font-bold">Logout</span>
-                </div>
-            </div>
+
+                    <nav data-dev-hint="second-main-navigation or footer navigation">
+                        <a href="#"
+                           className="block py-2 px-4 transition duration-200 hover:bg-gray-700 hover:text-white">
+                            Logout
+                        </a>
+                    </nav>
+                </aside>
         </>
     );
 }
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,12 +1,75 @@
 import type {NextPage} from 'next'
 import Sidebar from "../components/sidebar";
+import React from "react";
 
 
 const Home: NextPage = () => {
     return (
         <>
-            <Sidebar />
+            <div className="relative min-h-screen md:flex" data-dev-hint="container">
+                <Sidebar/>
+                <main id="content" className="flex-1 p-6 lg:px-8">
+                    <div className="max-w-7xl mx-auto">
+                        <div className="px-4 py-6 sm:px-0">
+                            <h2 className="my-6 text-2xl font-bold text-gray-700 dark:text-gray-200">Dashboard</h2>
+                            <div className="grid gap-6 mb-8 md:grid-cols-2 xl:grid-cols-4">
+                                <div className="flex items-center p-4 bg-white rounded-lg shadow-xs dark:bg-[#1a1c23]">
+                                    <div
+                                        className="p-3 mr-4 text-blue-500 bg-blue-100 rounded-full dark:text-blue-100 dark:bg-blue-500">
+                                        <svg className="w-5 h-5" fill="currentColor" xmlns="http://www.w3.org/2000/svg"
+                                             viewBox="0 0 640 512">
+                                            <path
+                                                d="M172.5 131.1C228.1 75.51 320.5 75.51 376.1 131.1C426.1 181.1 433.5 260.8 392.4 318.3L391.3 319.9C381 334.2 361 337.6 346.7 327.3C332.3 317 328.9 297 339.2 282.7L340.3 281.1C363.2 249 359.6 205.1 331.7 177.2C300.3 145.8 249.2 145.8 217.7 177.2L105.5 289.5C73.99 320.1 73.99 372 105.5 403.5C133.3 431.4 177.3 435 209.3 412.1L210.9 410.1C225.3 400.7 245.3 404 255.5 418.4C265.8 432.8 262.5 452.8 248.1 463.1L246.5 464.2C188.1 505.3 110.2 498.7 60.21 448.8C3.741 392.3 3.741 300.7 60.21 244.3L172.5 131.1zM467.5 380C411 436.5 319.5 436.5 263 380C213 330 206.5 251.2 247.6 193.7L248.7 192.1C258.1 177.8 278.1 174.4 293.3 184.7C307.7 194.1 311.1 214.1 300.8 229.3L299.7 230.9C276.8 262.1 280.4 306.9 308.3 334.8C339.7 366.2 390.8 366.2 422.3 334.8L534.5 222.5C566 191 566 139.1 534.5 108.5C506.7 80.63 462.7 76.99 430.7 99.9L429.1 101C414.7 111.3 394.7 107.1 384.5 93.58C374.2 79.2 377.5 59.21 391.9 48.94L393.5 47.82C451 6.731 529.8 13.25 579.8 63.24C636.3 119.7 636.3 211.3 579.8 267.7L467.5 380z"></path>
+                                        </svg>
 
+                                    </div>
+                                    <div>
+                                        <p className="mb-2 text-sm font-medium text-gray-600 dark:text-gray-400">
+                                            Links
+                                        </p>
+                                        <p className="text-lg font-semibold text-gray-700 dark:text-gray-200">
+                                            0 </p>
+                                    </div>
+                                </div>
+                                <div className="flex items-center p-4 bg-white rounded-lg shadow-xs dark:bg-[#1a1c23]">
+                                    <div
+                                        className="p-3 mr-4 text-blue-500 bg-blue-100 rounded-full dark:text-blue-100 dark:bg-blue-500">
+                                        <svg className="w-5 h-5" fill="currentColor" xmlns="http://www.w3.org/2000/svg"
+                                             viewBox="0 0 640 512">
+                                            <path
+                                                d="M319.9 320c57.41 0 103.1-46.56 103.1-104c0-57.44-46.54-104-103.1-104c-57.41 0-103.1 46.56-103.1 104C215.9 273.4 262.5 320 319.9 320zM369.9 352H270.1C191.6 352 128 411.7 128 485.3C128 500.1 140.7 512 156.4 512h327.2C499.3 512 512 500.1 512 485.3C512 411.7 448.4 352 369.9 352zM512 160c44.18 0 80-35.82 80-80S556.2 0 512 0c-44.18 0-80 35.82-80 80S467.8 160 512 160zM183.9 216c0-5.449 .9824-10.63 1.609-15.91C174.6 194.1 162.6 192 149.9 192H88.08C39.44 192 0 233.8 0 285.3C0 295.6 7.887 304 17.62 304h199.5C196.7 280.2 183.9 249.7 183.9 216zM128 160c44.18 0 80-35.82 80-80S172.2 0 128 0C83.82 0 48 35.82 48 80S83.82 160 128 160zM551.9 192h-61.84c-12.8 0-24.88 3.037-35.86 8.24C454.8 205.5 455.8 210.6 455.8 216c0 33.71-12.78 64.21-33.16 88h199.7C632.1 304 640 295.6 640 285.3C640 233.8 600.6 192 551.9 192z"></path>
+                                        </svg>
+                                    </div>
+                                    <div>
+                                        <p className="mb-2 text-sm font-medium text-gray-600 dark:text-gray-400">
+                                            Users
+                                        </p>
+                                        <p className="text-lg font-semibold text-gray-700 dark:text-gray-200">
+                                            1 </p>
+                                    </div>
+                                </div>
+                                <div className="flex items-center p-4 bg-white rounded-lg shadow-xs dark:bg-[#1a1c23]">
+                                    <div
+                                        className="p-3 mr-4 text-blue-500 bg-blue-100 rounded-full dark:text-blue-100 dark:bg-blue-500">
+                                        <svg className="w-5 h-5" fill="currentColor" xmlns="http://www.w3.org/2000/svg"
+                                             viewBox="0 0 384 512">
+                                            <path
+                                                d="M0 352c0 88.38 71.63 160 160 160h64c88.38 0 160-71.63 160-160V224H0V352zM176 0H160C71.63 0 0 71.62 0 160v32h176V0zM224 0h-16v192H384V160C384 71.62 312.4 0 224 0z"></path>
+                                        </svg>
+                                    </div>
+                                    <div>
+                                        <p className="mb-2 text-sm font-medium text-gray-600 dark:text-gray-400">
+                                            Total Clicks
+                                        </p>
+                                        <p className="text-lg font-semibold text-gray-700 dark:text-gray-200">
+                                            0 </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </main>
+            </div>
         </>
     )
 }
