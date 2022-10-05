14344b3baf4c679801843f7eb1d1e921669cc952
Optischa <fabian.evers2602@gmail.com>
2022-10-05 16:08:50 +0200

    Update user design

--- frontend/pages/user.tsx
+++ frontend/pages/user.tsx
@@ -1,8 +1,40 @@
 import {NextPage} from "next";
 import Sidebar from "../components/sidebar";
-import React from "react";
+import React, {useEffect, useState} from "react";
+import {useRouter} from "next/router";
+import {getCookie, removeCookies} from "cookies-next";
 
 const User: NextPage = () => {
+    const router = useRouter()
+    const [users, setUsers] = useState([
+        {
+            id: 1,
+            username: "Test",
+            email: "test@test.de",
+            status: "0",
+            role: "admin"
+        }
+    ]);
+
+    const headers = new Headers()
+    headers.append("Authorization", "Bearer " + getCookie("IHpKXQWXjx") as string)
+
+    useEffect(() => {
+        fetch("/api/user/validation", {
+            headers: headers,
+            method: "GET",
+        }).then(value => {
+            if (value.status === 200) {
+                fetch("/api/user", {
+                    headers: headers,
+                    method: "GET"
+                }).then(value => value.json()).then(value => setUsers(value))
+            } else {
+                router.push("/logout")
+            }
+        })
+    }, [])
+
     return (
         <>
             <div className="relative min-h-screen md:flex" data-dev-hint="container">
@@ -13,23 +45,70 @@ const User: NextPage = () => {
                             <h2 className="my-6 text-2xl font-bold text-gray-700 dark:text-gray-200">User</h2>
                             <div className="w-full overflow-hidden rounded-lg shadow-xs">
                                 <div className="w-full overflow-x-auto">
-                                    <table className="w-full whitespace-no-wrap">
-                                        <thead>
-                                        <tr className="text-xs font-semibold tracking-wide text-left text-gray-500 uppercase border-b dark:border-gray-700 bg-gray-50 dark:text-gray-400 dark:bg-[#1a1c23]">
-                                            <th className="px-4 py-3">Username</th>
-                                            <th className="px-4 py-3">Action</th>
-                                        </tr>
-                                        </thead>
-                                        <tbody className="bg-white divide-y dark:divide-gray-700 dark:bg-[#1a1c23]">
-
-                                        </tbody>
-                                    </table>
-                                </div>
-                                <div
-                                    className="grid px-4 py-3 text-xs font-semibold tracking-wide text-gray-500 uppercase border-t dark:border-gray-700 bg-gray-50 sm:grid-cols-9 dark:text-gray-400 dark:bg-[#1a1c23]">
-                <span className="flex items-center col-span-3">
-                  Showing 0 of 0                </span>
-                                    <span className="col-span-2"></span>
+                                    {users.map(value => {
+                                        return (
+                                            <>
+                                                <div
+                                                    className="p-4 text-gray-500 uppercase bg-gray-50 dark:text-gray-400 dark:bg-[#1a1c23]">
+                                                    <table className="w-full">
+                                                        <tbody>
+                                                        <tr>
+                                                            <td className="w-12">
+                                                                <img
+                                                                    aria-hidden="true"
+                                                                    className="hidden w-7 h-7 rounded-2xl object-cover w-full h-full dark:block"
+                                                                    src="/profil-dark.png"
+                                                                    alt="Office"
+                                                                />
+                                                            </td>
+                                                            <td className="font-bold">{value.username}</td>
+                                                            <td className="text-center">{value.role}</td>
+                                                            <td>
+                                                                {value.status === "0" ?
+                                                                    <>
+                                                                        <button
+                                                                            className="bg-red-500 rounded-2xl p-1 float-right mx-3 inline-block">
+                                                                            <svg xmlns="http://www.w3.org/2000/svg"
+                                                                                 className="w-5 p-0.5 h-5 fill-white"
+                                                                                 stroke="currentColor"
+                                                                                 viewBox="0 0 384 512">
+                                                                                <path
+                                                                                    d="M376.6 84.5c11.3-13.6 9.5-33.8-4.1-45.1s-33.8-9.5-45.1 4.1L192 206 56.6 43.5C45.3 29.9 25.1 28.1 11.5 39.4S-3.9 70.9 7.4 84.5L150.3 256 7.4 427.5c-11.3 13.6-9.5 33.8 4.1 45.1s33.8 9.5 45.1-4.1L192 306 327.4 468.5c11.3 13.6 31.5 15.4 45.1 4.1s15.4-31.5 4.1-45.1L233.7 256 376.6 84.5z"/>
+                                                                            </svg>
+                                                                        </button>
+                                                                        <button
+                                                                            className="bg-green-500 rounded-2xl p-1 float-right mx-3 inline-block">
+                                                                            <svg xmlns="http://www.w3.org/2000/svg"
+                                                                                 className="w-5 p-0.5 h-5 fill-white"
+                                                                                 stroke="currentColor"
+                                                                                 viewBox="0 0 512 512">
+                                                                                <path
+                                                                                    d="M470.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L192 338.7 425.4 105.4c12.5-12.5 32.8-12.5 45.3 0z"/>
+                                                                            </svg>
+                                                                        </button>
+                                                                    </>
+                                                                    :
+                                                                    <>
+                                                                        <button
+                                                                            className="bg-red-500 rounded-2xl p-1 float-right mx-3 inline-block">
+                                                                            <svg xmlns="http://www.w3.org/2000/svg"
+                                                                                 className="w-5 p-0.5 h-5 fill-white"
+                                                                                 stroke="currentColor"
+                                                                                 viewBox="0 0 448 512">
+                                                                                <path
+                                                                                    d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z"/>
+                                                                            </svg>
+                                                                        </button>
+                                                                    </>
+                                                                }
+                                                            </td>
+                                                        </tr>
+                                                        </tbody>
+                                                    </table>
+                                                </div>
+                                            </>
+                                        )
+                                    })}
                                 </div>
                             </div>
                         </div>
